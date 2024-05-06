1513105ed2c46bf4f7e51721ad8714ef0ff481db
Esteban <baab2an@live.fr>
2024-05-06 12:02:55 +0200

    Feat composant de texte réutilisable Typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { Seo } from "@/ui/components/seo";
+import { Typography } from "@/ui/design-systeme/typography/typography";
 
 export default function Home() {
   return (
     <>
       <Seo title="Coders Baban" description="Coders Baban" />
-      <h1>Baban le big boss du game</h1>
+      <Typography variant="display" component="h1">
+        Coders Baban
+      </Typography>
     </>
   );
 }
