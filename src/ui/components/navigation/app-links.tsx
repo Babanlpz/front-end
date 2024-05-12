350c0332c9896423c7b45eb3ecf83ad7b4763bf5
Esteban <baab2an@live.fr>
2024-05-12 23:09:22 +0200

    FEAT app links footer

---
+++ src/ui/components/navigation/app-links.tsx
@@ -0,0 +1,22 @@
+const footerApplicationLinks = [
+  {
+    label: "Acceuil",
+    baseUrl: "/",
+    type: "internal",
+  },
+  {
+    label: "Projets",
+    baseUrl: "/#",
+    type: "infernal",
+  },
+  {
+    label: "Coders Baban",
+    baseUrl: "/#",
+    type: "infernal",
+  },
+  {
+    label: "Informations",
+    baseUrl: "https://www.linkedin.com/in/estebanlopezdev/",
+    type: "external",
+  },
+];
