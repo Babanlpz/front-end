e60fb2bba33767da4f60fec504eeaef892730de1
Esteban <baab2an@live.fr>
2024-05-15 10:03:47 +0200

    FEAT dossier landing-page-view et landing-page-container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 /** COMPONENTS   */
 import { Layout } from "@/ui/components/layout/layout";
 import { Seo } from "@/ui/components/seo/seo";
+import { LandingPageContainer } from "@/ui/modules/landing-page/landing-page.container";
 
 export default function Home() {
   return (
     <>
       <Seo title="Coders Baban" description="Coders Baban" />
 
-      <Layout>Hello world</Layout>
+      <Layout>
+        <LandingPageContainer />
+      </Layout>
     </>
   );
 }
---
+++ src/ui/modules/landing-page/landing-page.view.tsx
@@ -0,0 +1,7 @@
+export const LandingPageView = () => {
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+};
