1dd4fee6a699042f350bf2eb7358247dbc20dd72
Esteban <baab2an@live.fr>
2024-05-15 15:59:50 +0200

    FEAT current course cta view

---
+++ src/ui/modules/landing-page/components/current-course-cta/current-course-cta.view.tsx
@@ -0,0 +1,50 @@
+import { Container } from "@/ui/components/container/container";
+import { Typography } from "@/ui/design-systeme/typography/typography";
+import Image from "next/image";
+import { RiPlayCircleLine } from "react-icons/ri";
+
+export const CurrentCourseCtaView = () => {
+  return (
+    <>
+      <div className="bg-gray-300">
+        <Container className="py-24 text-center">
+          <Typography variant="h2" component="h2" className="mb-2.5">
+            Formations React.js gratuite
+          </Typography>
+          <Typography variant="lead" component="h3" className="mb-5">
+            Apprends à coder l'app des singes codeurs
+          </Typography>
+          <Typography
+            variant="caption3"
+            theme="gray"
+            component="p"
+            className="pb-16"
+          >
+            Si tu veux un CV qui déchire, suis cette formation complète !
+          </Typography>
+          <a href="/#" target="_blank" className="">
+            <div className="relative bg-gray-400 rounded h-[626px]">
+              <div className="relative  z-10 flex flex-col items-center justify-center gap-2 h-full text-white rounded bg-gray opacity-0 hover:opacity-95 animate">
+                <RiPlayCircleLine size={42} />
+                <Typography
+                  variant="caption2"
+                  theme="white"
+                  className="uppercase"
+                  weight="medium"
+                >
+                  Lire la formation
+                </Typography>
+              </div>
+              <Image
+                fill
+                src="./assets/images/xxx.jpeg"
+                alt=""
+                className="object-cover object-center rounded"
+              />
+            </div>
+          </a>
+        </Container>
+      </div>
+    </>
+  );
+};
--- src/ui/modules/landing-page/landing-page.view.tsx
+++ src/ui/modules/landing-page/landing-page.view.tsx
@@ -1,6 +1,7 @@
 import { FeaturedView } from "./components/featured/featured.view";
 import { HeroTopView } from "./components/hero-top/hero-top.view";
 import { CodersBabanSlackView } from "./components/coders-baban-slack/coders-baban-slack.view";
+import { CurrentCourseCtaView } from "./components/current-course-cta/current-course-cta.view";
 
 export const LandingPageView = () => {
   return (
@@ -8,6 +9,7 @@ export const LandingPageView = () => {
       <HeroTopView />
       <FeaturedView />
       <CodersBabanSlackView />
+      <CurrentCourseCtaView />
     </>
   );
 };
