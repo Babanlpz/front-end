c845137a72914bb72bde0b49b3cb098c203c9407
Esteban <baab2an@live.fr>
2024-05-17 15:32:07 +0200

    FEAT box pour formulaire design

--- src/ui/modules/authentification/register/register.view.tsx
+++ src/ui/modules/authentification/register/register.view.tsx
@@ -1,7 +1,23 @@
+import { Container } from "@/ui/components/container/container";
+import { Box } from "@/ui/design-systeme/box/box";
+import Image from "next/image";
+
 export const RegisterView = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <Container className="grid grid-cols-2 gap-20 mb-32">
+      <div className="">
+        <div className="relative w-full h-[531px] ">
+          <Image
+            fill
+            src="/assets/images/register.jpg"
+            alt="Description de l'lilustration..."
+            className="object-scale-down"
+          />
+        </div>
+      </div>
+      <div className=" flex items-center">
+        <Box padding_y="py-5"> du contenu</Box>
+      </div>
+    </Container>
   );
 };
