8094d17acae125a688bdb0e85f076a29723146c5
Esteban <baab2an@live.fr>
2024-05-17 17:31:51 +0200

    FIX bug breadcrumbs pas aligner

--- src/ui/components/container/container.tsx
+++ src/ui/components/container/container.tsx
@@ -9,13 +9,12 @@ export const Container = ({ children, className }: Props) => {
   return (
     <>
       <div
-        className={clsx(
-          "w-full max-w-7xl mx-auto px-5 space-y-5 lg:px-10",
-          className
-        )}
+        className={clsx("w-full max-w-7xl mx-auto px-5 lg:px-10", className)}
       >
         {children}
       </div>
     </>
   );
 };
+
+// TODO : space-y-5 dans le className retirer pour que le breadcrumb soit aligné avec le contenu.
