356b9ae728a1a6435745560ac0bd22e222429a84
Esteban <baab2an@live.fr>
2024-05-20 10:35:48 +0200

    FEAT authentification firebase

--- src/ui/modules/authentification/register/register.container.tsx
+++ src/ui/modules/authentification/register/register.container.tsx
@@ -2,7 +2,7 @@ import { RegisterFormFieldsType } from "@/types/forms";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { RegisterView } from "./register.view";
-import { set } from "firebase/database";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const RegisterContainer = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -15,9 +15,24 @@ export const RegisterContainer = () => {
     reset,
   } = useForm<RegisterFormFieldsType>();
 
+  const handleCreateUserAuthentication = async ({
+    email,
+    password,
+    how_did_hear,
+  }: RegisterFormFieldsType) => {};
+
   const onSubmit: SubmitHandler<RegisterFormFieldsType> = async (formData) => {
     setIsLoading(true);
-    console.log(formData);
+    const { password } = formData;
+
+    if (password.length <= 5) {
+      setError("password", {
+        type: "manual",
+        message: "Ton mot de passe doit contenir au moins 6 caractères.",
+      });
+      setIsLoading(false);
+      return;
+    }
   };
 
   return (
