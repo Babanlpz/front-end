5c8103db0531978873a258ab9c29d63e2b2b5205
Esteban <baab2an@live.fr>
2024-05-21 10:28:31 +0200

    FEAT button variant alert-danger

---
+++ src/pages/mon-espace/index.tsx
@@ -0,0 +1,13 @@
+/** COMPONENTS   */
+import { Layout } from "@/ui/components/layout/layout";
+import { Seo } from "@/ui/components/seo/seo";
+
+export default function Connexion() {
+  return (
+    <>
+      <Seo title="Mon espace" description="Description de la page" />
+
+      <Layout></Layout>
+    </>
+  );
+}
---
+++ src/ui/modules/user-profil/user-account/user-account.container.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/ui/design-systeme/button/button";
+
+export const UserAccountContainer = () => {
+  return (
+    <div>
+      <Button action={} variant="">
+        Déconnexion
+      </Button>
+    </div>
+  );
+};
