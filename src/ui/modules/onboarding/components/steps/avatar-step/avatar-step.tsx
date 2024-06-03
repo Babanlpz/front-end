7dd0bd4cc8876859172e8fbc3810a97f44b09dd5
Esteban <baab2an@live.fr>
2024-06-03 10:56:58 +0200

    FEAT function qui permet d'enregistrer la photo de profil dans le dossier de l'utilisateur firebase

--- src/ui/modules/onboarding/components/steps/avatar-step/avatar-step.tsx
+++ src/ui/modules/onboarding/components/steps/avatar-step/avatar-step.tsx
@@ -1,10 +1,21 @@
+import { updateUserIdentificationData } from "@/api/authentication";
+import { firestoreUpdateDocument } from "@/api/firestore";
+import { storage } from "@/config/firebase-config";
 import { useAuth } from "@/context/AuthUserContext";
 import { useToggle } from "@/hooks/use-toggle";
 import { BaseCompponentProps } from "@/types/onboarding-steps-list";
 import { Container } from "@/ui/components/container/container";
 import { UploadAvatar } from "@/ui/components/upload-avatar/upload-avatar";
 import { Typography } from "@/ui/design-systeme/typography/typography";
+import {
+  StorageReference,
+  UploadTask,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { OnboardingFooter } from "../../footer/onboarding-footer";
 import { OnboardingTabs } from "../../tabs/onboarding-tabs.tsx";
 
@@ -16,11 +27,14 @@ export const AvatarStep = ({
   getCurrentStep,
 }: BaseCompponentProps) => {
   const { authUser } = useAuth();
-  const { value: isLoading, setValue: setLoading } = useToggle();
+  const { value: isLoading, toggle } = useToggle();
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | ArrayBuffer | null>(
     null
   );
+
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
   const handleImageSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -34,6 +48,64 @@ export const AvatarStep = ({
         setImagePreview(imgDataUrl);
       };
       reader.readAsDataURL(file);
+    }
+  };
+
+  const updateUserDocument = async (photoURL: string) => {
+    const body = {
+      photoURL: photoURL,
+    };
+    await updateUserIdentificationData(authUser?.uid, body);
+
+    const { error } = await firestoreUpdateDocument(
+      "users",
+      authUser?.uid,
+      body
+    );
+    if (error) {
+      toggle();
+      toast.error(
+        "Une erreur est survenue lors de la mise à jour de votre photo de profil"
+      );
+      return;
+    }
+    toggle();
+    next();
+  };
+
+  // en francais : "Une erreur est survenue lors de l'envoi de l'image"
+  const handleUploadImage = async () => {
+    let storageRef: StorageReference;
+    let uploadTask: UploadTask;
+
+    if (selectedImage !== null) {
+      toggle();
+      storageRef = ref(
+        storage,
+        `user-medias/${authUser?.uid}/avatar/avatar-${authUser?.uid}`
+      );
+      uploadTask = uploadBytesResumable(storageRef, selectedImage);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setUploadProgress(progress);
+        },
+        (error) => {
+          console.error(error);
+          toggle();
+          toast.error("Une erreur est survenue lors de l'envoi de l'image");
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            // console.log(":: downloadURL ::", downloadURL);
+          });
+        }
+      );
+    } else {
+      next();
     }
   };
 
@@ -59,15 +131,17 @@ export const AvatarStep = ({
             </div>
             <div className="flex items-center h-full col-span-6">
               <div className="flex justify-center w-full">
-                <UploadAvatar handleImageSelect={handleImageSelect} 
-                imagePreview={imagePreview}/>
+                <UploadAvatar
+                  handleImageSelect={handleImageSelect}
+                  imagePreview={imagePreview}
+                />
               </div>
             </div>
           </Container>
         </div>{" "}
         <OnboardingFooter
           prev={prev}
-          next={next}
+          next={handleUploadImage}
           isFinalStep={isFinalStep}
           isLoading={isLoading}
         />
