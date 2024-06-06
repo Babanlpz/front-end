a0f0f5c57427cfa9d8f24775867d75a13731abd4
Esteban <baab2an@live.fr>
2024-06-06 10:05:49 +0200

    FEAT call to action side bar dans mon espace

--- src/ui/components/layout/layout.tsx
+++ src/ui/components/layout/layout.tsx
@@ -2,6 +2,7 @@ import { SessionStatusTypes } from "@/types/session-status-types";
 import { Breadcrumbs } from "@/ui/components/breadcrumbs/breadcrumbs";
 import { Session } from "@/ui/session/session";
 import React from "react";
+import { CallsToActionSideBarContribution } from "../calls-to-action/calls-to-action-side-bar-contribution";
 import { Container } from "../container/container";
 import { Footer } from "../navigation/footer";
 import { Navigation } from "../navigation/navigation";
@@ -26,8 +27,9 @@ export const Layout = ({
     view = (
       <Container className="mb-14">
         <div className="grid grid-cols-12 gap-7">
-          <div className="col-span-3">
+          <div className="col-span-3 space-y-8">
             <UserAccountNavigation />
+            <CallsToActionSideBarContribution />
           </div>
           <div className="col-span-9">{children}</div>
         </div>
