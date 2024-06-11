30e1c5f1563bce62f442b88fd95bd79f9e1511fa
Esteban <baab2an@live.fr>
2024-06-11 12:39:51 +0200

    FEAT next export config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["images.unsplash.com", "www.freepik.com", "www.unsplash.com"],
+    domains: [
+      "images.unsplash.com",
+      "www.freepik.com",
+      "www.unsplash.com",
+      "www.pexels.com",
+      "www.pixabay.com",
+    ],
     unoptimized: true,
   },
   reactStrictMode: false,
